30798d1cf9f03f15e2561d1e7eea811e420aa594
Ben Sullivan <bensully513@gmail.com>
2018-04-09 15:56:05 -0600

    updated db

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function findById (data, id) {
     } 
 }
 
-app.listen(3000, () => {
-    console.log(`Listening on port 3000`)
+app.listen(3060, () => {
+    console.log(`Listening on port 3060`)
 })
 
 app.get('/', (req, res, next) => {
